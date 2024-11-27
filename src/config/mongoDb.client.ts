1dfcbc35f8d2d115118dbc91a2b4210299798947
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-11-28 04:13:31 +0700

    error congifg db schema

--- src/config/mongoDb.client.ts
+++ src/config/mongoDb.client.ts
@@ -5,17 +5,19 @@ dotenv.config();
 
 let isConnected = false;
 
+
+// Publish
+
 export async function connectToDatabase(): Promise<void> {
     if (isConnected) {
         return;
     }
 
-    const url: string = process.env.MongoDB_cloud2 || '';
-    const dbName = "MarFlix_Movie";
-    const fullUrl = `${url}/${dbName}`;
+    const url: string = process.env.MongoDB_cloud2 || "";
+    console.log("Connecting to MongoDB with URL:", url);
 
     try {
-        await mongoose.connect(fullUrl);
+        await mongoose.connect(url);
         isConnected = true;
         console.log("MongoDB connected successfully with mongoose");
     } catch (error) {
@@ -23,3 +25,28 @@ export async function connectToDatabase(): Promise<void> {
         throw error;
     }
 }
+
+
+
+
+
+// Local 
+
+// export async function connectToDatabase(): Promise<void> {
+//     if (isConnected) {
+//         return;
+//     }
+
+//     const url: string = process.env.MongoDB_Local || '';
+//     const dbName = "MarFlix_Movie";
+//     const fullUrl = `${url}/${dbName}`;
+
+//     try {
+//         await mongoose.connect(fullUrl);
+//         isConnected = true;
+//         console.log("MongoDB connected successfully with mongoose");
+//     } catch (error) {
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// }
