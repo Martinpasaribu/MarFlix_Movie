29cebb503d3783db585e0f6157a9232d8ea1a7d7
Martinpasaribu <83689864+Martinpasaribu@users.noreply.github.com>
2024-11-28 03:10:17 +0700

    error depedendi react v 05

--- src/app/Provider/LayoutProvider/page.tsx
+++ src/app/Provider/LayoutProvider/page.tsx
@@ -1,15 +1,16 @@
 // components/LayoutWrapper.tsx
 "use client";
 
-
 import { usePathname } from "next/navigation";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 export default function LayoutClient({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
-  const noLayoutPaths = ["/auth","/question"];
-  const hideLayout = noLayoutPaths.includes(pathname);
+  const noLayoutPaths = ["/auth", "/question"];
+
+  // Periksa apakah pathname dimulai dengan salah satu path di noLayoutPaths
+  const hideLayout = noLayoutPaths.some((path) => pathname.startsWith(path));
 
   return (
     <>
